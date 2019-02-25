4dc0961310869a4a1a7c3b94913328dccca5ce09
Arnaud Renaud <arnaud.renaud@gmail.com>
2019-02-25 16:43:00 +0100

    Add queries to insert restaurants in db

--- populate-db.js
+++ populate-db.js
@@ -1,26 +1,42 @@
 require('dotenv').config();
-const mysql = require('mysql');
+const { Client } = require('pg');
 
 const {
   CREATE_AREAS_TABLE_QUERY,
   INSERT_AREAS_QUERY,
+  CREATE_RESTAURANTS_TABLE_QUERY,
+  INSERT_RESTAURANTS_TABLE_QUERY,
 } = require('./sql-queries');
 
-const connection = mysql.createConnection({
+const client = new Client({
   host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-connection.connect();
-
-connection.query(CREATE_AREAS_TABLE_QUERY, function(error, results) {
-  if (error) throw error;
-  console.log('Created table areas', results);
-});
-connection.query(INSERT_AREAS_QUERY, function(error, results) {
-  if (error) throw error;
-  console.log('Inserted areas', results);
 });
 
-connection.end();
+async function runQuery(query, name) {
+  try {
+    await client.query(query);
+    console.log(`${name}: success`);
+  } catch (error) {
+    console.log(`${name}: error: ${error}`);
+  }
+}
+
+const runQueries = async () => {
+  await client.connect();
+  await runQuery(CREATE_AREAS_TABLE_QUERY, 'CREATE_AREAS_TABLE_QUERY');
+  await runQuery(INSERT_AREAS_QUERY, 'INSERT_AREAS_QUERY');
+  await runQuery(
+    CREATE_RESTAURANTS_TABLE_QUERY,
+    'CREATE_RESTAURANTS_TABLE_QUERY'
+  );
+  await runQuery(
+    INSERT_RESTAURANTS_TABLE_QUERY,
+    'INSERT_RESTAURANTS_TABLE_QUERY'
+  );
+  await client.end();
+};
+
+runQueries();
