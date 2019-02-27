3a1b1433beed51c7d12c1b6c524fd5c01f70ac9a
Arnaud Renaud <arnaud.renaud@gmail.com>
2019-02-27 10:31:00 +0100

    Serve client build with Express server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+
 const apiRouter = require('./api-router');
 const { dbClient } = require('./setup-db');
+
+const PATH_TO_STATIC_DIR = 'client/build';
 
 dbClient.connect();
 
 const app = express();
 app.use('/api', apiRouter);
+app.use(express.static(path.join(__dirname, PATH_TO_STATIC_DIR)));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, PATH_TO_STATIC_DIR, 'index.html'));
+});
 
 app.listen(
   process.env.PORT || 3001,
